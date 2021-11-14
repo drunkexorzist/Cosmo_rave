2229670f61926a3831f0377dd892f723341a1635
Юрий <70635903+drunkexorzist@users.noreply.github.com>
2021-11-14 22:09:27 +1000

    Передвижение пресонажа

--- main.py
+++ main.py
@@ -1,13 +1,87 @@
-import pygame as py
+import pygame
 
-py.init()
+pygame.init()
 
-window = py.display.set_mode((800, 600))
+screen_width = 1920
+screen_height = 1080
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("COSMO RAVE")
+
+clock = pygame.time.Clock()
+FPS = 60
+
+moving_left = False
+moving_right = False
+
+bg_img = (61, 95, 107)
+
+def draw_bg():
+    screen.fill(bg_img)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, char_type, x, y, scale, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.char_type = char_type
+        img = pygame.image.load(f"img/{self.char_type}/dont_walk/1.png")
+        self.direction = 1
+        self.flip = False
+        self.speed = speed
+        self.image = pygame.transform.scale(img,
+                                            (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def move(self, moving_left, moving_right):
+        dx = 0
+        dy = 0
+
+        if moving_left:
+            dx -= self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx -= -self.speed
+            self.flip = False
+            self.direction = 1
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def draw(self):
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+
+player = Player('player', 400, 400, 0.1, 5)
 
 while True:
-    for event in py.event.get():
-        if event.type == py.QUIT:
-            exit()
 
-py.quit()
+    clock.tick(FPS)
+
+    draw_bg()
+
+    player.draw()
+    player.move(moving_left, moving_right)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_ESCAPE:
+                exit()
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+
+    pygame.display.update()
+
+pygame.quit()
 quit()
