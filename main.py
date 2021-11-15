446b3ed2be636fbea86bd6b31a9e28fa450c2e8f
Юрий <70635903+drunkexorzist@users.noreply.github.com>
2021-11-15 22:31:28 +1000

    Анимация ходьбы и прыжка

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 
@@ -11,25 +12,48 @@ pygame.display.set_caption("COSMO RAVE")
 clock = pygame.time.Clock()
 FPS = 60
 
+GRAVITY = 0.75
+
 moving_left = False
 moving_right = False
 
-bg_img = (61, 95, 107)
+bg = (61, 95, 107)
+red = (255, 0, 0)
+
 
 def draw_bg():
-    screen.fill(bg_img)
+    screen.fill(bg)
+    pygame.draw.line(screen, red, (0, 600), (screen_width, 600))
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
+        self.alive = True
         self.char_type = char_type
-        img = pygame.image.load(f"img/{self.char_type}/dont_walk/1.png")
-        self.direction = 1
-        self.flip = False
         self.speed = speed
-        self.image = pygame.transform.scale(img,
-                                            (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.direction = 1
+        self.vel_y = 0
+        self.jump = False
+        self.in_air = True
+        self.flip = False
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+
+        animation_types = ['dont_walk', 'move_left', 'move_right', 'very_dont_walk']
+        for animation in animation_types:
+            temp_list = []
+            num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
+            for i in range(num_of_frames):
+                img = pygame.image.load(f"img/{self.char_type}/{animation}/{i}.png")
+                img = pygame.transform.scale(img,
+                                             (int(img.get_width() * scale), int(img.get_height() * scale)))
+                temp_list.append(img)
+            self.animation_list.append(temp_list)
+
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
@@ -38,19 +62,55 @@ class Player(pygame.sprite.Sprite):
         dy = 0
 
         if moving_left:
-            dx -= self.speed
+            dx = -self.speed
             self.flip = True
             self.direction = -1
         if moving_right:
-            dx -= -self.speed
+            dx = self.speed
             self.flip = False
             self.direction = 1
+
+        if self.jump == True and self.in_air == False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+            self.vel_y
+        dy += self.vel_y
+
+        # Коллизия с поверхностью
+        if self.rect.bottom + dy > 620:
+            dy = 620 - self.rect.bottom
+            self.in_air = False
 
         self.rect.x += dx
         self.rect.y += dy
 
-    def draw(self):
-        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+    def update_animation(self):
+        ANIMATION_COOLDOWN = 200  # Скорость анимации
+
+        self.image = self.animation_list[self.action][self.frame_index]
+
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+    def update_action(self, new_action):
+        if new_action != self.action:
+            self.action = new_action
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
+
+    def draw(self, moving_left, moving_right):
+        if moving_left or moving_right:
+            screen.blit(self.image, self.rect)
+        else:
+            screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
 
 player = Player('player', 400, 400, 0.1, 5)
@@ -61,8 +121,19 @@ while True:
 
     draw_bg()
 
-    player.draw()
-    player.move(moving_left, moving_right)
+    player.update_animation()
+    player.draw(moving_left, moving_right)
+
+    if player.alive:
+        if player.in_air:
+            player.update_action(3)
+        elif moving_left:
+            player.update_action(1)
+        elif moving_right:
+            player.update_action(2)
+        else:
+            player.update_action(3)
+        player.move(moving_left, moving_right)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -72,6 +143,8 @@ while True:
                 moving_left = True
             if event.key == pygame.K_d:
                 moving_right = True
+            if event.key == pygame.K_w and player.alive:
+                player.jump = True
             if event.key == pygame.K_ESCAPE:
                 exit()
 
